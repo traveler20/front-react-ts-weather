ec736299a9f7db0a6b7b69582eefcba290203a8e
tabito.otsubo <tabibito.traveler.sea.1039@gmail.com>
2021-08-10 08:29:00 +0900

    🎉Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Title from "./components/Title";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Hello react</h1>
+			<Title />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Title.tsx
@@ -0,0 +1,5 @@
+const Title = () => {
+	return <h1>ワールドウェザー</h1>;
+};
+
+export default Title;
