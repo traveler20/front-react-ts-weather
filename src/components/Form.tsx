a12b8ddff91029cb290eb3469072f080ed617e29
tabito.otsubo <tabibito.traveler.sea.1039@gmail.com>
2021-08-10 11:03:21 +0900

    📝Modify Form.tsx v3

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,16 +3,26 @@ import { useState } from "react";
 const Form = () => {
 	const [city, setCity] = useState<string>("");
 
+	const getWeather = (e: any) => {
+		e.preventDefault();
+		fetch(
+			"https://api.weatherapi.com/v1/current.json?key=4a5058a46c0b439689911920211008&q=London&aqi=no"
+		)
+			.then((res) => res.json())
+			.then((data) => console.log(data));
+	};
+
 	return (
 		<form>
 			<input
-				type="test"
+				type="text"
 				name="city"
 				placeholder="都市名"
 				onChange={(e) => setCity(e.target.value)}
 			/>
-			{city}
-			<button type="submit">Get Weather</button>
+			<button type="submit" onClick={getWeather}>
+				Get Weather
+			</button>
 		</form>
 	);
 };
