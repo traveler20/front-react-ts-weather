992c8604a6bc51effbaac69e4c6b919e47a87541
tabito.otsubo <tabibito.traveler.sea.1039@gmail.com>
2021-08-10 12:03:21 +0900

    📝Modify Results.tsx v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import Title from "./components/Title";
 import Form from "./components/Form";
+import Results from "./components/Results";
 import "./App.css";
 
 function App() {
+	// ▼Form.tsxより追記
+	const [city, setCity] = useState<string>("");
+
+	const getWeather = (e: any) => {
+		e.preventDefault();
+		fetch(
+			"https://api.weatherapi.com/v1/current.json?key=4a5058a46c0b439689911920211008&q=London&aqi=no"
+		)
+			.then((res) => res.json())
+			.then((data) => console.log(data));
+	};
+	// ▲Form.tsxより追記
+
 	return (
 		<div className="App">
 			<h1>Hello react</h1>
 			<Title />
-			<Form />
+			<Form setCity={setCity} getWeather={getWeather} />
+			<Results />
 		</div>
 	);
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,26 +1,18 @@
-import { useState } from "react";
+type FormPropsType = {
+	setCity: React.Dispatch<React.SetStateAction<string>>;
+	getWeather: (e: any) => void;
+};
 
-const Form = () => {
-	const [city, setCity] = useState<string>("");
-
-	const getWeather = (e: any) => {
-		e.preventDefault();
-		fetch(
-			"https://api.weatherapi.com/v1/current.json?key=4a5058a46c0b439689911920211008&q=London&aqi=no"
-		)
-			.then((res) => res.json())
-			.then((data) => console.log(data));
-	};
-
+const Form = (props: FormPropsType) => {
 	return (
 		<form>
 			<input
 				type="text"
 				name="city"
 				placeholder="都市名"
-				onChange={(e) => setCity(e.target.value)}
+				onChange={(e) => props.setCity(e.target.value)}
 			/>
-			<button type="submit" onClick={getWeather}>
+			<button type="submit" onClick={props.getWeather}>
 				Get Weather
 			</button>
 		</form>
